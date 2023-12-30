a0b1f137e47262bbe9260627754a97c99b54ad7d
plaze <banikakash131@gamil.com>
2023-12-30 22:55:39 +0530

    feat: add api

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 
-const PORT = 3500 || process.PORT;
+const PORT = 3500 || process.env.PORT;
 
+app.get("/api", require("./routes/api/user"));
 app.listen(PORT, () => {
-  console(`server running on port ${PORT}`);
+  console.log(`server running on port ${PORT}`);
 });
