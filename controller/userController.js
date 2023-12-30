eb157ab8a8186fcef211bad7741adf0797789eee
ShatadalDas <shatadaldas2020@gmail.com>
2023-12-31 02:17:08 +0530

    feat: add new route to get a particular user

--- controller/userController.js
+++ controller/userController.js
@@ -1,13 +1,23 @@
 const model = require("./../model/user.json");
 
 const getAll = async (req, res) => {
-  const user = await model;
+  const users = await model;
   try {
-    if (!user) return res.status(204).json({ message: "user not found" });
+    if (!users) return res.status(204).json({ message: "no user found" });
+    res.json(users);
+  } catch (err) {
+    console.err(err.message);
+  }
+};
+
+const getOne = async (req, res) => {
+  const user = await model.filter((user) => user.id == req.query.id)
+  try {
+    if (!user.length) return res.json({ message: "user not found" });
     res.json(user);
   } catch (err) {
     console.err(err.message);
   }
 };
 
-module.exports = { getAll };
+module.exports = { getAll, getOne };
--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const user = require("../controller/userController");
 
 router.route("/getAll").get(user.getAll);
+router.route("/get").get(user.getOne);
 
 module.exports = router;
