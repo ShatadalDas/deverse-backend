815888f003fea7028f052b20c38d6ca86b7c875a
plaze <banikakash131@gamil.com>
2023-12-30 23:34:28 +0530

    feat: add userController

---
+++ controller/userController.js
@@ -0,0 +1,12 @@
+const model = require("./../model/user.json");
+
+const getAllUser = async (req, res) => {
+  const user = await model;
+  try {
+    if (!user) return res.status(204).json({ message: "user not found" });
+    res.json(user);
+  } catch (err) {
+    console.err(err.message);
+  }
+};
+getAllUser();
